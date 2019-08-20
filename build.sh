25e9175df11a9c118f7a91b25c5639b762e7e0ec
陈哈哈 <yoojiachen@gmail.com>
2019-08-20 14:30:30 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-GOOS=linux GOARCH=arm make image push
-GOOS=linux GOARCH=arm64 make image push
-GOOS=linux GOARCH=amd64 make image push
-GOOS=linux make manifest
+SUDO="sudo "
+if [ "Darwin" == "$(uname -s)" ]; then
+    SUDO=""
+fi
+
+OS_SUDO=${SUDO} GOOS=linux GOARCH=arm make image push
+OS_SUDO=${SUDO} GOOS=linux GOARCH=arm64 make image push
+OS_SUDO=${SUDO} GOOS=linux GOARCH=amd64 make image push
+OS_SUDO=${SUDO} GOOS=linux make manifest
